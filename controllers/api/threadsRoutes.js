7498485bf5d45d48d59a81d630fe5c0220fa0281
murpheycm <cmmurphey@gmail.com>
2023-08-24 19:48:29 -0400

    Updated files within controllers folder

---
+++ controllers/api/threadsRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Threads } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newThread = await Threads.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newThread);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const threadsData = await Threads.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!threadsData) {
+      res.status(404).json({ message: 'No thread found with this id!' });
+      return;
+    }
+
+    res.status(200).json(threadsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
