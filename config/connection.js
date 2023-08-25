074d1f99a9e2c3fa1b6ee7a9686e126ffca6aeb9
murpheycm <cmmurphey@gmail.com>
2023-08-24 20:59:07 -0400

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.threads_db) {
-  sequelize = new Sequelize(process.env.threads_db);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
