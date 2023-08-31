e575f5cfcb1655eba91bed3b07e3ab1e95533ed5
murpheycm <cmmurphey@gmail.com>
2023-08-30 23:59:47 -0400

    Update to ghost

--- public/js/ghost.js
+++ public/js/ghost.js
@@ -1,10 +1,15 @@
-function ShowHideDiv(btnGhost) {
-    var GhostStyle = document.getElementById("ghostanimation");
-    if (btnGhost.value == "Hide Ghost") {
-            GhostStyle.style.display = "none";
-            btnGhost.value = "Play Ghost";
-    } else {
-            GhostStyle.style.display = "block";
-            btnGhost.value = "Hide Ghost";
-        }
-}
+document.addEventListener("DOMContentLoaded", function () {
+        const btnGhost = document.getElementById("btnGhost");
+        // const GhostStyle = document.getElementById("ghostanimation");
+
+        btnGhost.addEventListener("click", () => {
+                console.log("button clicked!");
+                // if (GhostStyle.style.display === "none") {
+                //         GhostStyle.style.display = "block";
+                //         btnGhost.innerHTML = "Hide";
+                // } else {
+                //         GhostStyle.style.display = "none";
+                //         btnGhost.innerHTML = "Play Ghost";
+                // }
+        });
+});
