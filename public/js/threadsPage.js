72e2302cdf50250d279e32749b3ad20635bd8874
Shabadoo9 <brinstonzbpe@gmail.com>
2023-09-04 16:34:47 -0400

    didnt work

--- public/js/threadsPage.js
+++ public/js/threadsPage.js
@@ -1,32 +1,33 @@
-// threadsPage.js
 
 // Select elements
 const addCommentButton = document.getElementById("add-comment-button");
 const commentBox = document.getElementById("comment-box");
 const commentTextarea = document.getElementById("comment-textarea");
 const submitCommentButton = document.getElementById("submit-comment-button");
-const commentsList = document.querySelector(".comments-list");
 
 // Add a click event listener to the "Add Comment" button
 addCommentButton.addEventListener("click", () => {
   // Toggle the visibility of the comment box
   if (commentBox.style.display === "block") {
-    commentBox.style.display = "none";
+    commentBox.style.display = "none"; // Hide the comment box if it's shown
   } else {
-    commentBox.style.display = "block";
+    commentBox.style.display = "block"; // Show the comment box if it's hidden
   }
 });
 
-// Add a click event listener to the "Submit" button for toggling comment box
+// Add a click event listener to the "Submit" button
 submitCommentButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const commentText = commentTextarea.value;
-  console.log("Comment:", commentText);
-  commentTextarea.value = "";
-  commentBox.style.display = "none";
+  e.preventDefault(); // Prevent the form from submitting (you can send the data to your server here)
 
-  // Add the new comment to the comments list
-  const commentItem = document.createElement("li");
-  commentItem.textContent = commentText;
-  commentsList.appendChild(commentItem);
+  // Get the comment text from the textarea
+  const commentText = commentTextarea.value;
+
+  // Do something with the comment text (e.g., send it to the server)
+  console.log("Comment:", commentText);
+
+  // Clear the textarea
+  commentTextarea.value = "";
+
+  // Hide the comment box
+  commentBox.style.display = "none";
 });
