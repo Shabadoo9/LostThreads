922bc4a9d6623d26ee19b8999612f5e87de15f30
Shabadoo9 <brinstonzbpe@gmail.com>
2023-09-04 10:38:38 -0400

    debug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -91,7 +91,7 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/createThread', async (req, res) => {
+router.get('/createThread', withAuth, async (req, res) => {
   try {
    res.render('createThread'); 
   } catch (err) {
