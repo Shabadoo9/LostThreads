a7ab954d10c7dfca91a070e2bd8848e36651cc48
Shabadoo9 <brinstonzbpe@gmail.com>
2023-09-04 16:43:43 -0400

    comment section v2

--- public/js/threadsPage.js
+++ public/js/threadsPage.js
@@ -1,33 +1,56 @@
-
 // Select elements
-const addCommentButton = document.getElementById("add-comment-button");
-const commentBox = document.getElementById("comment-box");
 const commentTextarea = document.getElementById("comment-textarea");
 const submitCommentButton = document.getElementById("submit-comment-button");
+const commentsList = document.querySelector(".comments-list");
 
-// Add a click event listener to the "Add Comment" button
-addCommentButton.addEventListener("click", () => {
-  // Toggle the visibility of the comment box
-  if (commentBox.style.display === "block") {
-    commentBox.style.display = "none"; // Hide the comment box if it's shown
-  } else {
-    commentBox.style.display = "block"; // Show the comment box if it's hidden
+// Function to render comments
+function renderComments(comments) {
+  commentsList.innerHTML = "";
+  comments.forEach((comment) => {
+    const commentItem = document.createElement("div");
+    commentItem.classList.add("comment");
+    commentItem.innerHTML = `
+      <p><strong>${comment.User.name}</strong></p>
+      <p>${formatDate(comment.date_created)}</p>
+      <p>${comment.description}</p>
+    `;
+    commentsList.appendChild(commentItem);
+  });
+}
+
+// Add a click event listener to the "Submit" button
+submitCommentButton.addEventListener("click", async () => {
+  const commentText = commentTextarea.value;
+
+  // Create a new comment on the server and fetch updated comments
+  const response = await fetch("/api/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ description: commentText }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    const newComments = data.comments;
+    renderComments(newComments);
+    commentTextarea.value = "";
   }
 });
 
-// Add a click event listener to the "Submit" button
-submitCommentButton.addEventListener("click", (e) => {
-  e.preventDefault(); // Prevent the form from submitting (you can send the data to your server here)
+// Function to format dates (replace with your date formatting function)
+function formatDate(dateString) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+}
 
-  // Get the comment text from the textarea
-  const commentText = commentTextarea.value;
-
-  // Do something with the comment text (e.g., send it to the server)
-  console.log("Comment:", commentText);
-
-  // Clear the textarea
-  commentTextarea.value = "";
-
-  // Hide the comment box
-  commentBox.style.display = "none";
-});
+// Fetch and render comments on page load
+(async () => {
+  const response = await fetch("/api/comments");
+  if (response.ok) {
+    const data = await response.json();
+    const comments = data.comments;
+    renderComments(comments);
+  }
+})();
