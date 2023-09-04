9d6fde8254c303a5d4e43744a87c152bb90088a6
Shabadoo9 <brinstonzbpe@gmail.com>
2023-09-04 17:06:44 -0400

    comment debug

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,12 @@ const userRoutes = require('./userRoutes');
 const threadsRoutes = require('./threadsRoutes');
 const profileRoutes = require('./profileRoutes');
 const createThreadRoutes = require('./createThreadRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
 router.use('/threads', threadsRoutes);
 router.use('/create-profile', profileRoutes);
 router.use('/create-thread', createThreadRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
