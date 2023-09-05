4db0737ff225c78b75c6b379d84289bb826c178e
Shabadoo9 <brinstonzbpe@gmail.com>
2023-09-05 01:03:18 -0400

    debug

--- public/js/threadsPage.js
+++ public/js/threadsPage.js
@@ -2,70 +2,75 @@ const addCommentButton = document.getElementById("add-comment-button");
 const commentBox = document.getElementById("comment-box");
 const commentTextarea = document.getElementById("comment-textarea");
 const submitCommentButton = document.getElementById("submit-comment-button");
-const commentsList = document.querySelector(".comments-list"); // Add this line
+const commentsList = document.querySelector(".comments-list");
 
-// Function to render comments
-function renderComments(comments) {
-  commentsList.innerHTML = "";
-  comments.forEach((comment) => {
-    const commentItem = document.createElement("div");
-    commentItem.classList.add("comment");
-    commentItem.innerHTML = `
-      <p><strong>${comment.User.name}</strong></p>
-      <p>${formatDate(comment.date_created)}</p>
-      <p>${comment.description}</p>
-    `;
-    commentsList.appendChild(commentItem);
+document.addEventListener("DOMContentLoaded", () => {
+  // Function to render comments
+  function renderComments(comments) {
+    const commentsList = document.querySelector(".comments-list");
+
+    // Create an HTML string for the comments
+    const commentsHTML = comments.map((comment) => `
+      <div class="comment">
+        <p><strong>${comment.User.name}</strong></p>
+        <p>${formatDate(comment.date_created)}</p>
+        <p>${comment.description}</p>
+      </div>
+    `).join('');
+
+    // Set the inner HTML of the comments list
+    commentsList.innerHTML = commentsHTML;
+  }
+
+  // Add a click event listener to the "Add Comment" button
+  const addCommentButton = document.getElementById("add-comment-button");
+  addCommentButton.addEventListener("click", () => {
+    // Toggle the visibility of the comment box
+    const commentBox = document.getElementById("comment-box");
+    commentBox.style.display = commentBox.style.display === "block" ? "none" : "block";
   });
-}
 
-// Add a click event listener to the "Add Comment" button
-addCommentButton.addEventListener("click", () => {
-  // Toggle the visibility of the comment box
-  if (commentBox.style.display === "block") {
-    commentBox.style.display = "none"; // Hide the comment box if it's shown
-  } else {
-    commentBox.style.display = "block"; // Show the comment box if it's hidden
-  }
-});
+  // Add a click event listener to the "Submit" button
+  const submitCommentButton = document.getElementById("submit-comment-button");
+  submitCommentButton.addEventListener("click", async (e) => {
+    e.preventDefault(); // Prevent the form from submitting (you can send the data to your server here)
 
-// Add a click event listener to the "Submit" button
-submitCommentButton.addEventListener("click", async (e) => {
-  e.preventDefault(); // Prevent the form from submitting (you can send the data to your server here)
+    // Get the comment text from the textarea
+    const commentText = commentTextarea.value;
 
-  // Get the comment text from the textarea
-  const commentText = commentTextarea.value;
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ description: commentText }),
+    });
+    console.log("Comment Text:", commentText);
+    if (response.ok) {
+      const data = await response.json();
+      const newComments = data.comments;
+      renderComments(newComments);
+      commentTextarea.value = "";
+    }
 
-  const response = await fetch("/api/comments", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ description: commentText }),
+    // Hide the comment box
+    const commentBox = document.getElementById("comment-box");
+    commentBox.style.display = "none";
   });
-  console.log("Comment Text:", commentText);
-  if (response.ok) {
-    const data = await response.json();
-    const newComments = data.comments;
-    renderComments(newComments);
-    commentTextarea.value = "";
+
+  // Function to format dates (replace with your date formatting function)
+  function formatDate(dateString) {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
   }
-  // Hide the comment box
-  commentBox.style.display = "none";
+
+  // Fetch and render comments on page load
+  (async () => {
+    const response = await fetch("/api/comments");
+    if (response.ok) {
+      const data = await response.json();
+      const comments = data.comments;
+      renderComments(comments);
+    }
+  });
 });
-
-// Function to format dates (replace with your date formatting function)
-function formatDate(dateString) {
-  const options = { year: "numeric", month: "long", day: "numeric" };
-  return new Date(dateString).toLocaleDateString(undefined, options);
-}
-
-// Fetch and render comments on page load
-(async () => {
-  const response = await fetch("/api/comments");
-  if (response.ok) {
-    const data = await response.json();
-    const comments = data.comments;
-    renderComments(comments);
-  }
-})();
