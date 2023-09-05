118e85d12dfc9abbc85069e0ebba3dfbb51f06d4
Shabadoo9 <brinstonzbpe@gmail.com>
2023-09-05 10:59:03 -0400

    added get route for logged in user threads

--- controllers/api/threadsRoutes.js
+++ controllers/api/threadsRoutes.js
@@ -1,49 +1,63 @@
 const router = require('express').Router();
-const routes = require('../');
 const { User, Threads } = require('../../models');
 const withAuth = require('../../utils/auth');
-// const multer = require('multer');
-const path = require('path');
 
-
-
-
-router.post('/', withAuth, async (req, res) => { 
-  
+// Your existing route for creating a new thread
+router.post('/', withAuth, async (req, res) => {
   try {
-    const newThreads = await Threads.create({
+    const newThread = await Threads.create({
       ...req.body,
       user_id: req.session.user_id,
-      // image: req.file.filename,
     });
     
-    res.render('homepage')
+    res.render('homepage');
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-
-
+// Your existing route for deleting a thread
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const threadsData = await Threads.destroy({
+    const threadData = await Threads.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!threadsData) {
-      res.status(404).json({ message: 'No threads found with this id!' });
+    if (!threadData) {
+      res.status(404).json({ message: 'No thread found with this id!' });
       return;
     }
 
-    res.status(200).json(threadsData);
+    res.status(200).json(threadData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// New route for fetching threads belonging to the logged-in user
+router.get('/my-threads', withAuth, async (req, res) => {
+  try {
+    const userId = req.session.user_id;
+
+    const userThreads = await Threads.findAll({
+      where: {
+        user_id: userId,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    res.render('your_template', { threads: userThreads });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
